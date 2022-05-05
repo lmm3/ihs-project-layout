e3cbb7de125640798ba9e16f45e5e297fe587ded
mvbs3 <mvbs3@cin.ufpe.br>
2022-05-05 14:50:36 -0300

    reading input

--- app/src/snake.py
+++ app/src/snake.py
@@ -63,7 +63,7 @@ class Snake(object):
         #moveFPGAU = #receber click cima do fpga Aqui
         #moveFPGAD = #receber click baixo do fpga Aqui
         #Move Left
-        print(red)
+        print(int(red))
         '''
         if (red == hex(0xE) AND flagMove==0){
             flagMove = 1
