ead973a087066d916eddc036d55663f20dbcdb1f
mvbs3 <mvbs3@cin.ufpe.br>
2022-05-05 18:07:09 -0300

    reading input

--- app/src/snake.py
+++ app/src/snake.py
@@ -143,13 +143,13 @@ class Snake(object):
                 if i == len(self.body)-1:
                     self.turns.pop(p)
             else:
-                if c.dirnx == -1 and c.pos[0] <=0:
+                if c.dirnx == -1 and c.pos[0] <0:
                     c.pos = (c.rows-1,c.pos[1])  
-                elif c.dirnx == 1 and c.pos[0] >=c.rows-1:
+                elif c.dirnx == 1 and c.pos[0] >c.rows-1:
                     c.pos = (0,c.pos[1]) 
-                elif c.dirny == 1 and c.pos[1] >=c.rows-1:
+                elif c.dirny == 1 and c.pos[1] >c.rows-1:
                     c.pos = (c.pos[0],0) 
-                elif c.dirny == -1 and c.pos[1] <=0:
+                elif c.dirny == -1 and c.pos[1] <0:
                     c.pos = (c.pos[0],c.rows-1) 
                 else:
                     c.move(c.dirnx,c.dirny)
